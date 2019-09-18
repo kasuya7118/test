4348aa1e1ebf3449507934a14eabd86c02aca575
kasuya7118 <sk7118820g@gmail.com>
2019-09-18 15:28:23 +0900

    コメント

---
+++ htdocs/4each_keijiban/index.php
@@ -0,0 +1,113 @@
+  
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+ <meta charset="UTF-8">
+ <title>4eachblog 掲示板</title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+ 
+ <body>
+  <?php
+  mb_internal_encoding("utf8");
+  $pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","mysql");
+  $stmt= $pdo->query("select * from 4each_keijiban");
+ 
+  
+  ?>
+  
+  
+  <img src="4eachblog_logo.jpg">
+   <header>
+         
+         <ul>
+             <li>トップ</li>
+             <li>プロフィール</li>
+             <li>4eachについて</li>
+             <li>登録フォーム</li>
+             <li>問い合わせ</li>
+             <li>その他</li>
+         </ul>
+         
+     </header>
+  <main>
+<!--   <div class="main-container"></div>-->
+   <div class="left">
+      <h1>プログラミングに役立つ掲示板</h1>
+
+       <p>入力フォーム</p>
+       <form method="post"action="insert.php">
+
+        <div>
+       <label>ハンドルネーム</label>
+       <br>
+       <input type="text" class="text" size="35" name="handlename">
+        </div>
+
+        <div>
+      <lable>タイトル</lable>
+      <br>
+      <input type="text" class="text" size="35" name="title">
+       </div>
+
+        <div>
+        <lable>コメント</lable>
+        <br>
+        <textarea cols="35" rows="7" name="comments"></textarea>
+       </div>
+
+       <div>
+        <input type="submit" class="submit" value="投稿する">
+        </div>
+     </form>
+     
+    
+    
+    <?php
+    
+    while($row=$stmt->fetch()){
+     echo"<div class='kiji'>";
+     echo"<h3>".$row['title']."</h3>";
+     echo"<div class='contents'>";
+     echo $row['comments'];
+     echo"<div class='handlename'>posted by".$row['handlename']."</div>";
+     echo"</div>";
+     echo"</div>";
+    }
+    ?>
+    
+   
+   </div>
+   
+   <div class="right">
+             <ul><h2>人気の記事</h2>
+                 <li>PHP オススメ本</li>
+                 <li>PHP MyAdminの使い方</li>
+                 <li>今人気のエディタ Top5</li>
+                 <li>HTMLの基礎</li>
+             </ul>
+            
+                <ul><h2>オススメリンク</h2>
+                <li>インターノース株式会社</li>
+                <li>XAMPPのダウンロード</li>
+                <li>Eclipseのダウンロード</li>
+                <li>Brackets</li>
+             </ul>
+            
+                <ul><h2>カテゴリ</h2>
+                <li>HTML</li>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>javaScript</li>
+            </ul>
+    </div>  
+  </main>           
+ 
+  <footer>
+         Ccopyright © internous |4each blog is one which provides A to Z about programming.      
+  </footer>
+ </body>
+
+</html>
