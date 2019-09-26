d73aaf9d1c37380ef03b1ea017ae5f80c353a78a
kasuya7118 <sk7118820g@gmail.com>
2019-09-26 09:54:14 +0900

    コメント

--- TestDB/sql/testdb.sql
+++ TestDB/sql/testdb.sql
@@ -2,7 +2,7 @@ drop database if exists testdb;
 create database testdb;
 use testdb;
 
-create tabletest_table(
+create table test_table(
 user_id int,
 user_name varchar(255),
 password varchar(255)
